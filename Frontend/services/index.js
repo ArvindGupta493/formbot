52c97ec46570113948254bb7561b350b6417e167
Arvind Gupta <110850319+ArvindGupta493@users.noreply.github.com>
2025-01-23 19:08:46 +0530

    Update index.js

--- Frontend/services/index.js
+++ Frontend/services/index.js
@@ -1,4 +1,5 @@
-const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://formbot-navy.vercel.app";
+// const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "https://formbot-navy.vercel.app";
+const BACKEND_URL = import.meta.env.VITE_BACKEND_URL || "http://localhost:4000";
 
 // Backend services: User Registration
 export const userRegister = async (data) => {
